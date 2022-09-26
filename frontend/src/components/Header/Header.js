7f9f45edb68b4868f27732e04ace7bb595801625
krutarth45 <krutarth.798@gmail.com>
2022-09-26 15:37:01 +0530

    Added validation for headers

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import { Navbar, Nav, Form, Row, Col, Button } from 'react-bootstrap';
 import React from 'react';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Header.css';
 
 const Header = () => {
+  const schema = yup.object().shape({
+    email: yup.string().required('Email is required').email(),
+    password: yup.string().required('Password is required')
+  });
   return (
     <Navbar
       collapseOnSelect
@@ -30,33 +36,71 @@ const Header = () => {
         <Nav className="ms-auto">
           <div className="headerRight">
             <div className="headerRight__loginForm">
-              {/* <form>
-                <input type="text" name="email" placeholder="Email Address" />
-                <input type="password" name="password" placeholder="Password" />
-                <button id="headerForm__Login">Login</button>
-              </form> */}
-              <Form>
-                <Row>
-                  <Col>
-                    <Form.Control type="email" placeholder="Email" />
-                  </Col>
-                  <Col>
-                    <Form.Control type="password" placeholder="Password" />
-                  </Col>
-                  <Col>
-                    <Button
-                      style={{
-                        background: '#3b5998',
-                        border: 'none',
-                        fontWeight: '700'
-                      }}
-                      type="submit"
-                    >
-                      Login
-                    </Button>
-                  </Col>
-                </Row>
-              </Form>
+              <Formik
+                validationSchema={schema}
+                onSubmit={(values) => {
+                  console.log(values);
+                }}
+                initialValues={{
+                  email: '',
+                  password: ''
+                }}
+              >
+                {({ handleSubmit, handleChange, values, touched, errors }) => (
+                  <Form noValidate onSubmit={handleSubmit}>
+                    <Row>
+                      <Form.Group
+                        as={Col}
+                        controlId="validationFormik101"
+                        className="position-relative"
+                      >
+                        <Form.Control
+                          type="email"
+                          name="email"
+                          value={values.email}
+                          onChange={handleChange}
+                          isValid={touched.email && !errors.email}
+                          isInvalid={!!errors.email}
+                          placeholder="Email"
+                        />
+                        <Form.Control.Feedback type="invalid" tooltip>
+                          {errors.email}
+                        </Form.Control.Feedback>
+                      </Form.Group>
+                      <Form.Group
+                        as={Col}
+                        controlId="validationFormik102"
+                        className="position-relative"
+                      >
+                        <Form.Control
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                          value={values.password}
+                          onChange={handleChange}
+                          isValid={touched.password && !errors.password}
+                          isInvalid={!!errors.password}
+                        />
+                        <Form.Control.Feedback type="invalid" tooltip>
+                          {errors.password}
+                        </Form.Control.Feedback>
+                      </Form.Group>
+                      <Col>
+                        <Button
+                          style={{
+                            background: '#3b5998',
+                            border: 'none',
+                            fontWeight: '700'
+                          }}
+                          type="submit"
+                        >
+                          Login
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                )}
+              </Formik>
             </div>
             <div className="headerRight__modeShift">Go To Employer Sign In</div>
           </div>
