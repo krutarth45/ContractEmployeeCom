6e86bab389ecac691fceb8ff3c9fb8591bede126
krutarth45 <krutarth.798@gmail.com>
2022-09-27 13:39:49 +0530

    Added fraud and grievances page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './pages/Home';
 import Complaints from './components/HomePages/Complaints';
+import Grievance from './components/HomePages/Grievance';
+import Fraud from './components/HomePages/Fraud';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} exact />
       <Route path="/complaint" element={<Complaints />} />
+      <Route path="/grievance" element={<Grievance />} />
+      <Route path="/fraud-alert" element={<Fraud />} />
     </Routes>
   );
 };
---
+++ frontend/src/components/HomePages/Fraud.js
@@ -0,0 +1,31 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import './index.css';
+const Fraud = () => {
+  return (
+    <Container>
+      <Row className="text-center my-5">
+        <Col md={12}>
+          <h2 style={{ textDecoration: 'underline', fontWeight: 'bold' }}>
+            Welcome to Contractemploy.com Security Centre
+          </h2>
+        </Col>
+      </Row>
+      <Row className="text-center">
+        <Col md={12}>
+          <p className="fw-bold">
+            Please do not Pay any money or provide personal / financial
+            information (Credit/ debit card number, CVV number, OTP, etc.) to
+            anyone. Fraudsters may ask you to pay in the pretext of registration
+            fee, Refundable Fee, Document Processing fee or any other excuse.
+            Before your respond to any email / call, we suggest you to do a
+            discreet enquiry and be sure to verify the authenticity of the
+            person with whom you are interacting You can also write to us at
+            contact@dharmaworks.in
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Fraud;
