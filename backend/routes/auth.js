f90fb123f2b763b7ed7c17fb1002d7a3c87b966d
krutarth45 <krutarth.798@gmail.com>
2022-09-29 11:21:58 +0530

    setup auth middleware and route

---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const Contractor = require('../models/Contractor');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contractor = await Contractor.findById(req.user.id).select(
+      '-password'
+    );
+    res.json(contractor);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('server error');
+  }
+});
+module.exports = router;
