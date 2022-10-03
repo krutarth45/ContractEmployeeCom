a65698e29a05394091c26f372cee9e99992131f1
krutarth45 <krutarth.798@gmail.com>
2022-10-03 19:30:29 +0530

    form validation for user details

--- frontend/src/components/Contractor/ContractorDetails/ContractorDetails.js
+++ frontend/src/components/Contractor/ContractorDetails/ContractorDetails.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useRef, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { Typeahead } from 'react-bootstrap-typeahead';
@@ -12,7 +13,7 @@ import {
 } from '../../../data';
 import './ContractorDetails.css';
 
-const ContractorDetails = () => {
+const ContractorDetailsB = () => {
   const [totalExpYear, setTotalExpYear] = useState([]);
   const [relExpYear, setRelExpYear] = useState([]);
   const [skillInfo, setSkillInfo] = useState([]);
@@ -25,10 +26,16 @@ const ContractorDetails = () => {
   const [noticePeriod, setNoticePeriod] = useState([]);
   const [currentCity, setCurrentCity] = useState([]);
   const [preferredCities, setPreferredCities] = useState([]);
-  const [date, setDate] = useState(new Date());
+  const [bday, setBday] = useState(new Date());
   const [resume, setResume] = useState(null);
+  const [error, setError] = useState('');
   const ref = useRef();
   const ref2 = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      setError('');
+    }, 3000);
+  }, [error]);
   return (
     <div>
       <Container>
@@ -46,214 +53,256 @@ const ContractorDetails = () => {
           </Col>
         </Row>
         <div className="p-4 profileDetailsDiv h-auto">
-          <Row>
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Total Experience: </Form.Label>
-                <Typeahead
-                  id="basic-typeahead-total-exp-single"
-                  labelKey="totalExp"
-                  onChange={setTotalExpYear}
-                  options={totalExp}
-                  placeholder="Total Experience"
-                  selected={totalExpYear}
-                />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Relevant Experience: </Form.Label>
-                <Typeahead
-                  id="basic-typeahead-single-rel-exp-single"
-                  labelKey="relevantExp"
-                  onChange={setRelExpYear}
-                  options={totalExp}
-                  placeholder="Relevant Experience"
-                  selected={relExpYear}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Skills: </Form.Label>
-                <Typeahead
-                  multiple
-                  id="basic-typeahead-multiple"
-                  labelKey="skills"
-                  options={skills}
-                  placeholder="Skills"
-                  selected={skillInfo}
-                  ref={ref}
-                  onChange={(selections) => {
-                    setSkillInfo(selections);
-                    ref.current.toggleMenu();
-                  }}
-                />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group controlId="companyName">
-                <Form.Label>Current Company: </Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Company Name"
-                  value={companyName}
-                  onChange={(e) => setCompanyName(e.target.value)}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Current Job Type: </Form.Label>
-                <Typeahead
-                  id="basic-typeahead-current-job-type"
-                  labelKey="jobType"
-                  onChange={setJobType}
-                  options={['Permanent', 'Contract']}
-                  placeholder="Current Job Type"
-                  selected={jobType}
-                />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Notice Period: </Form.Label>
-                <Typeahead
-                  id="basic-typeahead-notice-period"
-                  labelKey="noticePeriod"
-                  onChange={setNoticePeriod}
-                  options={notice}
-                  placeholder="Notice Period"
-                  selected={noticePeriod}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Current Monthly Salary: </Form.Label>
-                <div className="d-flex align-items-center">
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (
+                preferredCities.length === 0 ||
+                skillInfo.length === 0 ||
+                totalExpYear.length === 0 ||
+                relExpYear.length === 0 ||
+                jobType.length === 0 ||
+                curMonCurr.length === 0 ||
+                curMonSal.length === 0 ||
+                expMonCurr.length === 0 ||
+                expMonSal.length === 0 ||
+                noticePeriod.length === 0 ||
+                currentCity.length === 0
+              ) {
+                return setError('Please select Value from the Dropdown Menu');
+              }
+              // call the handle function here
+              console.log('Success');
+            }}
+          >
+            <Row>
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Total Experience: </Form.Label>
                   <Typeahead
-                    id="basic-typeahead-current-month-salary"
-                    labelKey="curMonSal"
-                    onChange={setCurMonSal}
-                    options={salary}
-                    placeholder="Current Monthly Salary"
-                    selected={curMonSal}
+                    inputProps={{ required: true }}
+                    id="basic-typeahead-total-exp-single"
+                    labelKey="totalExp"
+                    onChange={setTotalExpYear}
+                    options={totalExp}
+                    placeholder="Total Experience"
+                    selected={totalExpYear}
                   />
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Relevant Experience: </Form.Label>
                   <Typeahead
-                    className="ms-3"
-                    id="basic-typeahead-current-sal-currency"
-                    labelKey="curMonCurr"
-                    onChange={setCurMonCurr}
-                    options={currency}
-                    placeholder="Current Salary Currency"
-                    selected={curMonCurr}
+                    inputProps={{ required: true }}
+                    id="basic-typeahead-single-rel-exp-single"
+                    labelKey="relevantExp"
+                    onChange={setRelExpYear}
+                    options={totalExp}
+                    placeholder="Relevant Experience"
+                    selected={relExpYear}
                   />
-                </div>
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Expected Monthly Salary: </Form.Label>
-                <div className="d-flex align-items-center">
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mt-2">
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Skills: </Form.Label>
                   <Typeahead
-                    id="basic-typeahead-exp-month-salary"
-                    labelKey="expMonSal"
-                    onChange={setExpMonSal}
-                    options={salary}
-                    placeholder="Expected Monthly Salary"
-                    selected={expMonSal}
+                    multiple
+                    inputProps={{
+                      required: skillInfo.length === 0 ? true : false
+                    }}
+                    id="basic-typeahead-multiple"
+                    labelKey="skills"
+                    options={skills}
+                    placeholder="Skills"
+                    selected={skillInfo}
+                    ref={ref}
+                    onChange={(selections) => {
+                      setSkillInfo(selections);
+                      ref.current.toggleMenu();
+                    }}
                   />
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group controlId="companyName">
+                  <Form.Label>Current Company: </Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Company Name"
+                    value={companyName}
+                    onChange={(e) => setCompanyName(e.target.value)}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mt-2">
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Current Job Type: </Form.Label>
                   <Typeahead
-                    className="ms-3"
-                    id="basic-typeahead-current-sal-currency"
-                    labelKey="expMonCurr"
-                    onChange={setExpMonCurr}
-                    options={currency}
-                    placeholder="Salary Currency"
-                    selected={expMonCurr}
+                    inputProps={{ required: true }}
+                    id="basic-typeahead-current-job-type"
+                    labelKey="jobType"
+                    onChange={setJobType}
+                    options={['Permanent', 'Contract']}
+                    placeholder="Current Job Type"
+                    selected={jobType}
                   />
-                </div>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Current City: </Form.Label>
-                <Typeahead
-                  id="basic-typeahead-current-city"
-                  labelKey="currentCity"
-                  onChange={setCurrentCity}
-                  options={cityNames}
-                  placeholder="Current City"
-                  selected={currentCity}
-                />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group>
-                <Form.Label>Preferred Cities: </Form.Label>
-                <Typeahead
-                  multiple
-                  id="basic-typeahead-preferred-cities"
-                  labelKey="preferredCities"
-                  options={cityNames}
-                  placeholder="Preferred Cities"
-                  selected={preferredCities}
-                  ref={ref2}
-                  onChange={(selections) => {
-                    setPreferredCities(selections);
-                    ref2.current.toggleMenu();
-                  }}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row className="mt-2">
-            <Col sm={12} md={6}>
-              <Form.Group controlId="duedate">
-                <Form.Label>Date of Birth:</Form.Label>
-                <Form.Control
-                  type="date"
-                  name="duedate"
-                  placeholder="Due date"
-                  value={date}
-                  onChange={(e) => setDate(e.target.value)}
-                />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group controlId="formFile" className="mb-3">
-                <Form.Label>Resume</Form.Label>
-                <Form.Control
-                  onChange={(e) => {
-                    setResume(e.target.files[0]);
-                  }}
-                  type="file"
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <div className="my-2 text-center">
-            <Button
-              className="btn px-3 py-1"
-              type="submit"
-              style={{ backgroundColor: '#3b5998' }}
-            >
-              <span>Save</span>
-            </Button>
-          </div>
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Notice Period: </Form.Label>
+                  <Typeahead
+                    inputProps={{ required: true }}
+                    id="basic-typeahead-notice-period"
+                    labelKey="noticePeriod"
+                    onChange={setNoticePeriod}
+                    options={notice}
+                    placeholder="Notice Period"
+                    selected={noticePeriod}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mt-2">
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Current Monthly Salary: </Form.Label>
+                  <div className="d-flex align-items-center">
+                    <Typeahead
+                      inputProps={{ required: true }}
+                      id="basic-typeahead-current-month-salary"
+                      labelKey="curMonSal"
+                      onChange={setCurMonSal}
+                      options={salary}
+                      placeholder="Current Monthly Salary"
+                      selected={curMonSal}
+                    />
+                    <Typeahead
+                      className="ms-3"
+                      inputProps={{ required: true }}
+                      id="basic-typeahead-current-sal-currency"
+                      labelKey="curMonCurr"
+                      onChange={setCurMonCurr}
+                      options={currency}
+                      placeholder="Current Salary Currency"
+                      selected={curMonCurr}
+                    />
+                  </div>
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Expected Monthly Salary: </Form.Label>
+                  <div className="d-flex align-items-center">
+                    <Typeahead
+                      inputProps={{ required: true }}
+                      id="basic-typeahead-exp-month-salary"
+                      labelKey="expMonSal"
+                      onChange={setExpMonSal}
+                      options={salary}
+                      placeholder="Expected Monthly Salary"
+                      selected={expMonSal}
+                    />
+                    <Typeahead
+                      className="ms-3"
+                      inputProps={{ required: true }}
+                      id="basic-typeahead-current-sal-currency"
+                      labelKey="expMonCurr"
+                      onChange={setExpMonCurr}
+                      options={currency}
+                      placeholder="Salary Currency"
+                      selected={expMonCurr}
+                    />
+                  </div>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mt-2">
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Current City: </Form.Label>
+                  <Typeahead
+                    inputProps={{ required: true }}
+                    id="basic-typeahead-current-city"
+                    labelKey="currentCity"
+                    onChange={setCurrentCity}
+                    options={cityNames}
+                    placeholder="Current City"
+                    selected={currentCity}
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Preferred Cities: </Form.Label>
+                  <Typeahead
+                    multiple
+                    inputProps={{
+                      required: preferredCities.length === 0 ? true : false
+                    }}
+                    id="basic-typeahead-preferred-cities"
+                    labelKey="preferredCities"
+                    options={cityNames}
+                    placeholder="Preferred Cities"
+                    selected={preferredCities}
+                    ref={ref2}
+                    onChange={(selections) => {
+                      setPreferredCities(selections);
+                      ref2.current.toggleMenu();
+                    }}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mt-2">
+              <Col sm={12} md={6}>
+                <Form.Group>
+                  <Form.Label>Date of Birth:</Form.Label>
+                  <Form.Control
+                    required
+                    type="date"
+                    name="bDay"
+                    placeholder="Birthday"
+                    value={bday}
+                    onChange={(e) => setBday(e.target.value)}
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm={12} md={6}>
+                <Form.Group controlId="formFile" className="mb-3">
+                  <Form.Label>Resume</Form.Label>
+                  <Form.Control
+                    required
+                    onChange={(e) => {
+                      setResume(e.target.files[0]);
+                    }}
+                    type="file"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <div className="my-2 text-center">
+              {error && <p className="text-danger">{error}</p>}
+              <Button
+                className="btn px-3 py-1"
+                type="submit"
+                style={{ backgroundColor: '#3b5998' }}
+              >
+                <span>Save</span>
+              </Button>
+            </div>
+          </Form>
         </div>
       </Container>
     </div>
   );
 };
 
-export default ContractorDetails;
+export default ContractorDetailsB;
