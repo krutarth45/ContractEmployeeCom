a8e8310fc84779f1f6111aea09ca65a47abb15a9
krutarth45 <krutarth.798@gmail.com>
2022-10-11 21:50:06 +0530

    cleaned code, post job req fixed, display jobs for contractor in progress.

--- frontend/src/pages/Employer/ListApplicants.js
+++ frontend/src/pages/Employer/ListApplicants.js
@@ -1,9 +1,11 @@
+import AppliedContractors from '../../components/Employer/AppliedContractors';
 import EmployerHeader from '../../components/Employer/EmployerHeader';
 
 const ListApplicants = () => {
   return (
     <div>
       <EmployerHeader />
+      <AppliedContractors />
     </div>
   );
 };
